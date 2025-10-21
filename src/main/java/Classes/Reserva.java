d3a276aa814587de3e885b65a929bf16b55db7f3
ernesolorzano <esolorzano@frsf.utn.edu.ar>
2025-10-21 12:49:18 -0300

    MODIFICACION de atributos de una clase, no se agrego nada

--- src/main/java/Classes/Reserva.java
+++ src/main/java/Classes/Reserva.java
@@ -14,6 +14,31 @@ public class Reserva {
     private Date fechaDesde;
     private Date fechaHasta;
     private String estado;
+    private String nombre;
+    private String apellido;
+    private int telefono;
+
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+
 
     public void setEstado(String estado) {
         this.estado = estado;
