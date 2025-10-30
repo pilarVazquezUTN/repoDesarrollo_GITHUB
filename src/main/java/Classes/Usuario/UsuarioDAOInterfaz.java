c0cf8b8a1f602a151411de7cddd062dbf4862893
Valentin <valedasilvacatela.vdc@gmail.com>
2025-10-30 15:48:48 -0300

    asd

--- src/main/java/Classes/Usuario/UsuarioDAOInterfaz.java
+++ src/main/java/Classes/Usuario/UsuarioDAOInterfaz.java
@@ -1,8 +1,9 @@
 package Classes.Usuario;
 
+import Classes.Excepciones.UsuarioNoExistenteException;
 import Classes.Usuario.UsuarioDTO;
 
 public interface UsuarioDAOInterfaz {
-    boolean buscarUsuario(UsuarioDTO usuarioDTO);
+    boolean buscarUsuario(UsuarioDTO usuarioDTO)  throws UsuarioNoExistenteException;
     boolean usuarioEncontrado(String nombreUsuario, String contrasenaUsuario);
 }
