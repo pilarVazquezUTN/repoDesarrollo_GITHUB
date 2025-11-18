85bcf58f338b6346df923a439ee36d14ae12c20d
Pilar Vazquez <vazquezayalapilarmartina@gmail.com>
2025-11-18 19:37:43 -0300

    nada

--- src/main/java/com/app/app.java
+++ src/main/java/com/app/app.java
@@ -1,5 +1,14 @@
 package com.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import Main.App;
+
+@SpringBootApplication
 public class app {
 
+    public static void main (String [] args){
+        SpringApplication.run(App.class,args);
+    }
+        
 }
