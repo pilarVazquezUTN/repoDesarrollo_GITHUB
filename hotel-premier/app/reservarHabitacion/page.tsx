a07091f68876f544f61a8d0a8502e0737b43548e
guillefornari <guillefornari8@gmail.com>
2025-11-19 15:46:20 -0300

    cambie para que se elimine de la tabla de reservarHabitacion

--- hotel-premier/app/reservarHabitacion/page.tsx
+++ hotel-premier/app/reservarHabitacion/page.tsx
@@ -3,8 +3,25 @@
 import { useState } from "react";
 import EstadoHabitacion from "../estadoHabitacion/page";
 
-export default function BuscarHuesped() {
+export default function ReservarHabitacion() {
+    const [filas, setFilas] = useState([
+        { id: 1, numero: 3, desde: "02/12/2024", hasta: "02/12/2024", checked: false },
+        { id: 2, numero: 3, desde: "03/12/2024", hasta: "03/12/2024", checked: false },
+        { id: 3, numero: 5, desde: "03/12/2024", hasta: "05/12/2024", checked: false },
+        { id: 4, numero: 8, desde: "01/12/2024", hasta: "01/12/2024", checked: false },
+    ]);
+    //FUNCION PARA SABER QUE FUNCIONES ESTAN TILDADAS PARA ELIMINAR
+    const toggleCheck = (id:number) => { //ACTUALIZO EL ESTADO DE MI CHECKBOX EN MI CODIGO PARA QUE SE SEPA QUE ESTA SELECCIONADA (PASA DE {... checked: false} A {... checked: true} )
+        setFilas(prev => //ESTADO DE LAS FILAS
+            prev.map(f => //RECORRE LA FILA Y LA ACTUALIZA DEPENDIENDO SI F (FILA CON DATOS) CUMPLE O NO CON LO SIGUIENTE
+                f.id === id ? { ...f, checked: !f.checked } : f // LA F EN LA QUE YO ESTOY PARADA TIENE EL MISMO ID QUE ESTOY BUSCANDO -> SI ENTONCES COPIO LOS DATOS DE F Y CAMBIO SU CHECK A TRUE {... checked: false}=>{... checked: true}
+            )
+        );
+    };
 
+    const eliminarSeleccionados = () => {
+        setFilas(prev => prev.filter(f => !f.checked)); //FILTER CREA UN NUEVO ARRAY CON LAS FILAS QUE TENGAN CHECKBOX FALSE (ES DECIR QUE QUITA LAS SELECCIONADAS)
+    };
     return (
         <main className="flex gap-8 px-6 py-6 items-start">
             <section className="flex-2 ">
@@ -26,34 +43,20 @@ export default function BuscarHuesped() {
                     <tbody>
                         {/* Repetí esta fila para cada resultado SON EJEMPLOS EN REALIDAD LA TABLA ES DINAMICA  EN BASE A LA BDD*/}
                         {/* el boton de eliminar cuando se haga la implementacion se puede mejorar*/}
-                        <tr className="bg-white hover:bg-indigo-100">
-                            <td className="p-3 border text-center"><input type="checkbox" name="seleccion" /></td>
-                            <td className="p-2 border text-center">3</td>
-                            <td className="p-2 border text-center">02/12/2024</td>
-                            <td className="p-2 border text-center">02/12/2024</td>
-                        </tr>
-                        <tr className="bg-white hover:bg-indigo-100">
-                            <td className="p-3 border text-center"><input type="checkbox" name="seleccion" /></td>
-                            <td className="p-3 border text-center">3</td>
-                            <td className="p-3 border text-center">03/12/2024</td>
-                            <td className="p-3 border text-center">03/12/2024</td>
-                        </tr>
-                        <tr className="bg-white hover:bg-indigo-100">
-                            <td className="p-3 border text-center"><input type="checkbox" name="seleccion" /></td>
-                            <td className="p-2 border text-center">5</td>
-                            <td className="p-2 border text-center">03/12/2024</td>
-                            <td className="p-2 border text-center">05/12/2024</td>
-                        </tr>
-                        <tr className="bg-white hover:bg-indigo-100">
-                            <td className="p-3 border text-center"><input type="checkbox" name="seleccion" /></td>
-                            <td className="p-3 border text-center">8</td>
-                            <td className="p-3 border text-center">01/12/2024</td>
-                            <td className="p-3 border text-center">01/12/2024</td>
-                        </tr>
+                        {filas.map(f => (
+                            <tr key={f.id} className="bg-white hover:bg-indigo-100">
+                                <td className="p-3 border text-center">
+                                    <input type="checkbox" checked={f.checked} onChange={() => toggleCheck(f.id)}/>
+                                </td>
+                                <td className="p-3 border text-center">{f.numero}</td>
+                                <td className="p-3 border text-center">{f.desde}</td>
+                                <td className="p-3 border text-center">{f.hasta}</td>
+                            </tr>
+                        ))}
                     </tbody>
                 </table>
                 {/* se podria buscar la forma de que apretando la casilla de eliminar automaticamente se eliminen sin necesidad del boton eliminar*/ }
-                <button className="block mx-auto mt-6 px-4 py-2 bg-indigo-950 text-white rounded hover:bg-indigo-800 transition">Eliminar</button>
+                <button onClick={eliminarSeleccionados} className="block mx-auto mt-6 px-4 py-2 bg-indigo-950 text-white rounded hover:bg-indigo-800 transition">Eliminar</button>
             </section>
         </main>
     );
