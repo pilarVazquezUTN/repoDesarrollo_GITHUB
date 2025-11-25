bed25047874afdb63cb93a1ce642f65b8ee2250d
Pilar Vazquez <vazquezayalapilarmartina@gmail.com>
2025-11-24 21:23:59 -0300

    agregue POST huesped

--- src/main/java/com/hotelPremier/repository/HuespedRepositoryDAO.java
+++ src/main/java/com/hotelPremier/repository/HuespedRepositoryDAO.java
@@ -7,12 +7,15 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.hotelPremier.classes.huesped.Huesped;
+import com.hotelPremier.classes.huesped.HuespedDTO;
+import com.hotelPremier.classes.huesped.HuespedID;
 
 @Repository
-public interface HuespedRepositoryDAO extends JpaRepository<Huesped,Long>{
+public interface HuespedRepositoryDAO extends JpaRepository<Huesped,HuespedID>{
 
     @Override
     List<Huesped> findAll();
     List<Huesped> findByHuespedID_Dni(String dni);
-    
+    //HuespedDTO addHuesped(HuespedDTO huespedDTO);  
+    Huesped save(Huesped huesped);
 }
